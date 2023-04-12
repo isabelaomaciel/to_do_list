f021f81b08fc88fc1022e48b4ad80567bca671f1
Isabela Oliveira Maciel <isabelasouxx@gmail.com>
2023-04-11 21:38:54 -0300

    refactory all

---
+++ lib/components/difficulty.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Difficulty extends StatelessWidget {
+  final int difficultyLevel;
+
+  const Difficulty({
+    required this.difficultyLevel,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (difficultyLevel >= 1) ? Colors.brown : Colors.brown[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (difficultyLevel >= 2) ? Colors.brown : Colors.brown[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (difficultyLevel >= 3) ? Colors.brown : Colors.brown[100],
+        ),
+        Icon(
+          Icons.star,
+          size: 15,
+          color: (difficultyLevel >= 4) ? Colors.brown : Colors.brown[100],
+        ),
+        Icon(Icons.star,
+            size: 15,
+            color: (difficultyLevel >= 5) ? Colors.brown : Colors.brown[100]),
+      ],
+    );
+  }
+}
---
+++ lib/components/task.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/components/difficulty.dart';
+
+class Task extends StatefulWidget {
+  final String name;
+  final String foto;
+  final int dificuldade;
+
+  const Task(this.name, this.foto, this.dificuldade, {Key? key})
+      : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: Colors.brown,
+            ),
+            height: 140,
+          ),
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white,
+                ),
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Colors.black12,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(4),
+                        child: Image.asset(
+                          widget.foto,
+                          height: 100,
+                          width: 72,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 200,
+                          child: Text(
+                            widget.name,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                        Difficulty(
+                          difficultyLevel: widget.dificuldade,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 52,
+                      width: 52,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              nivel++;
+                            });
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: const [
+                              Icon(Icons.arrow_drop_up),
+                              Text(
+                                'UP',
+                                style: TextStyle(fontSize: 12),
+                              )
+                            ],
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: 200,
+                      child: LinearProgressIndicator(
+                        color: Colors.white,
+                        value: (widget.dificuldade > 0)
+                            ? (nivel / widget.dificuldade) / 10
+                            : 1,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Text('Nivel: $nivel',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
