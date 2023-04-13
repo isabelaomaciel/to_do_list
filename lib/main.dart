6d27e0b541d0475a31f1ee843a9778832e8ab72d
Isabela Oliveira Maciel <isabelasouxx@gmail.com>
2023-04-12 21:53:37 -0300

    image and errobuilder

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const FormScreen(),
+      home: const InitialScreen(),
     );
   }
 }
